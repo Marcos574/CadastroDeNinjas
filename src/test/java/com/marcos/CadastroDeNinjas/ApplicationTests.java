a10025f0c009d6b82fc487f56d12af057e064a0e
Marcos574 <mvdeus574@gmail.com>
2026-03-16 21:06:27 -0300

    first commit

---
+++ src/test/java/com/marcos/CadastroDeNinjas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
